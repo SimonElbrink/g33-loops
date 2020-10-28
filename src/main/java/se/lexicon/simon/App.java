65d9fd1474f7d95e14f3e9015dbbbbfa4836327e
Simon Elbrink <simon.elbrink@lexicon.se>
2020-10-28 12:04:12 +0100

    While loop - demo

--- src/main/java/se/lexicon/simon/App.java
+++ src/main/java/se/lexicon/simon/App.java
@@ -1,13 +1,46 @@
 package se.lexicon.simon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.println("How old are you?");
+
+            int age = scanner.nextInt();
+
+            if (age >= 18) {
+                System.out.println("you are an adult!");
+            } else {
+                System.out.println("you are a child");
+                break;
+            }
+
+        }
+
     }
 }
+
+/*
+
+boolean isAdult = true;
+
+while(isAdult){
+        System.out.println("How old are you?");
+
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("you are an adult!");
+        } else {
+            System.out.println("you are a child");
+            isAdult = false;
+        }
+
+        }
+ */
